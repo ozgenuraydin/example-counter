f015d677d7030ccd444fa6bbedac1fc16cfcc296
ozgenuraydin <ozgenuraydnn@gmail.com>
2020-05-28 06:02:01 +0300

    no message

--- src/App.js
+++ src/App.js
@@ -2,25 +2,69 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+   constructor(props) {
+       super(props);
+       this.state={
+           count:0
+       }
+   }
+
+increment =()=>{
+       this.setState({
+           count:this.state.count+1
+       })
 }
+
+decrement =()=>{
+       this.setState({
+           count:this.state.count-1
+       })
+}
+    render(){
+        const {count}=this.state;
+        return (
+            <div className="App">
+                <h1>COUNTER APP</h1>
+                <p>{count} </p>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement} >-</button>
+            </div>
+        )
+    }
+}
+
+
+
+/*
+ //ortak bir fonksiyon ile  yazmak isteseydik:
+
+class App extends React.Component{
+   constructor(props) {
+       super(props);
+       this.state={
+           count:0
+       }
+   }
+
+degistir = (operation) =>{
+  this.setState({
+    count: operation == "azalt" ? this.state.count-1 : this.state.count+1
+})
+}
+    render(){
+        const {count}=this.state;
+        return (
+            <div className="App">
+                <h1>COUNTER APP</h1>
+                <p>{count} </p>
+                <button onClick={()=>{this.degistir("arttır")}}>+</button>
+                <button onClick={()=>{this.degistir("azalt")}}>-</button>
+            </div>
+        )
+    }
+
+}*/
 
 export default App;
